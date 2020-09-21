d030b002b73839cb68ba583a3fcb4f0f766c08ad
liuhangkai <x@12.com>
2020-09-21 11:26:52 +0800

    master

--- 1.go
+++ 1.go
@@ -2,23 +2,15 @@ package custframe
 
 import (
 	"fmt"
+
 	"github.com/liuhangkaixcode/custframe/tools"
 )
 
-func Hello()  {
-	s:=tools.GetString()
+func Hello() {
+	s := tools.GetString()
 	fmt.Println(s)
 }
 
-func V1()  {
-	fmt.Println("===========V1")
-}
-
-
-func V2()  {
-	fmt.Println("===========V2")
-}
-
-func V3()  {
-	fmt.Println("===========V3")
+func Master() {
+	fmt.Println("===========Master")
 }
